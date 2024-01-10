17f57a418add32b20272e0ce3ff63a1797c136c4
kridian <thekridian@proton.me>
2024-01-09 23:42:31 -0800

    Deposit WIP

--- programs/cercols/src/lib.rs
+++ programs/cercols/src/lib.rs
@@ -1,6 +1,9 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::Mint;
-// use mpl_token_metadata::accounts::Metadata;
+use anchor_spl::{
+    token::{Mint, TokenAccount, Token, Transfer, transfer}, 
+    metadata::{MetadataAccount, MasterEditionAccount, Metadata},
+    associated_token::AssociatedToken
+};
 
 declare_id!("FjZVUx8ergLAEaB1Mucqizhk9XtQTZpnAffS5FBT2h4c");
 
@@ -15,15 +18,19 @@ pub mod cercols {
         pool.collection_mint = ctx.accounts.collection_mint.key();
         pool.swap_fee_lamports = swap_fee_lamports;
         pool.size = 0;
-        msg!("ctx: {:?}", ctx.accounts.pool.collection_mint.to_string());
-
-        // let metadata_pubkey = Metadata::find_pda(&ctx.accounts.collection_mint.key());
-        // let metadata: Metadata = Metadata::try_from(metadata_pubkey.to_account_info());
+        pool.pool_bump = ctx.bumps.pool;
+        pool.nft_auth_bump = ctx.bumps.nft_authority;
         
         Ok(())
     }
 
     pub fn deposit(ctx: Context<Deposit>) -> Result<()> {
+        // Deposit collection items in the pool
+        let pool = &mut ctx.accounts.pool;
+        pool.size = pool.size.checked_add(1).unwrap();
+
+        transfer(ctx.accounts.deposit_nft_ctx(), 1)?;
+
         Ok(())
     }
 
@@ -42,11 +49,19 @@ pub mod cercols {
 
 #[account]
 pub struct PoolState { // 8
+    /// The creator of the pool (32)
     authority: Pubkey, // 32
+    /// The verified collection address of the NFT (32)
     collection_mint: Pubkey, // 32
+    /// The fee in lamports for swapping in the pool
     swap_fee_lamports: u64, // 8
+    /// The number of items currently in the pool
     size: u32, // 4
-    // TOTAL: 84
+    /// The bump of pool details PDA (1)
+    pool_bump: u8, // 1
+    /// The bump of NFT authority PDA (1)
+    nft_auth_bump: u8 // 1
+    // TOTAL: 86
 }
 
 #[derive(Accounts)]
@@ -57,7 +72,7 @@ pub struct InitPool<'info> {
         payer = user, 
         seeds = [b"cercols_pool", collection_mint.key().as_ref(), user.key().as_ref()], 
         bump, 
-        space = 84 
+        space = 86
     )]
     pub pool: Account<'info, PoolState>,
 
@@ -67,11 +82,94 @@ pub struct InitPool<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
 
+    /// CHECK: This account is not read or written
+    #[account(
+        seeds = [b"nft_authority", pool.key().as_ref()],
+        bump
+    )]
+    pub nft_authority: UncheckedAccount<'info>,
+
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Deposit {}
+pub struct Deposit<'info> {
+    #[account(
+        mut, 
+        seeds = [
+            b"cercols_pool", 
+            pool.collection_mint.as_ref(), 
+            pool.authority.as_ref()
+        ], 
+        bump = pool.pool_bump
+    )]
+    pub pool: Account<'info, PoolState>,
+
+    #[account(mint::decimals = 0, constraint = nft_mint.supply == 1)]
+    pub nft_mint: Account<'info, Mint>,
+
+    #[account(
+        mut, 
+        associated_token::mint = nft_mint, 
+        associated_token::authority = user, 
+        constraint = nft_token.amount == 1
+    )]
+    pub nft_token: Account<'info, TokenAccount>,
+    
+    #[account(
+        seeds = [b"metadata", Metadata::id().as_ref(), nft_mint.key().as_ref()],
+        seeds::program = Metadata::id(),
+        bump,
+        constraint = nft_metadata.collection.as_ref().unwrap().verified,
+        constraint = nft_metadata.collection.as_ref().unwrap().key == pool.collection_mint
+    )]
+    pub nft_metadata: Box<Account<'info, MetadataAccount>>,
+
+    #[account(
+        seeds = [b"metadata",
+            Metadata::id().as_ref(),
+            nft_mint.key().as_ref(),
+            b"edition"
+        ],
+        seeds::program = Metadata::id(),
+        bump
+    )]
+    pub nft_edition: Box<Account<'info, MasterEditionAccount>>,
+
+    /// CHECK: This account is not read or written
+    #[account(seeds = [b"nft_authority", pool.key().as_ref()], bump = pool.nft_auth_bump)]
+    pub nft_authority: UncheckedAccount<'info>,
+
+    #[account(
+        init,
+        payer = user, 
+        associated_token::mint = nft_mint, 
+        associated_token::authority = nft_authority
+    )]
+    pub nft_custody: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>
+
+}
+
+impl<'info> Deposit<'info> {
+    pub fn deposit_nft_ctx(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.nft_token.to_account_info(),
+            to: self.nft_custody.to_account_info(),
+            authority: self.user.to_account_info()
+        };
+
+        let cpi_program = self.token_program.clone().to_account_info();
+
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
 
 #[derive(Accounts)]
 pub struct Withdraw {}
--- tests/cercols.ts
+++ tests/cercols.ts
@@ -1,4 +1,5 @@
 import * as anchor from "@coral-xyz/anchor";
+import * as token from "@solana/spl-token";
 import { Program } from "@coral-xyz/anchor";
 import { Cercols } from "../target/types/cercols";
 import { expect } from "chai";
@@ -15,11 +16,36 @@ describe("cercols", () => {
 
   const [poolPda] = anchor.web3.PublicKey.findProgramAddressSync(
     [
-      Buffer.from("cercols_pool"),
+      anchor.utils.bytes.utf8.encode("cercols_pool"),
       collectionMint.toBuffer(),
       program.provider.publicKey.toBytes(),
     ],
     program.programId
+  );
+
+  const [nftAuthorityPda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [anchor.utils.bytes.utf8.encode("nft_authority"), poolPda.toBytes()],
+    program.programId
+  );
+
+  // NFT of the collection - must be owned by the Signer
+  const nftMint = new anchor.web3.PublicKey(
+    "DskQgewLBTmPBZwWAZ5U7swcPeggpZ5eRbb6gurY1oZd"
+  );
+  const nftToken = new anchor.web3.PublicKey(
+    "H8nooeBKDQTcp75zZoyBuMHiWjsWPWH42q3dWJn8CA3a"
+  );
+  const nftMetadata = new anchor.web3.PublicKey(
+    "9naYoZ4uZxCPPvQPnQwzmFFnFKwrLiUNLf7sQnqWr4RN"
+  );
+  const nftEdition = new anchor.web3.PublicKey(
+    "FkHtoHUk6kWehv6VrH1mdbgfBM7xxhmcpEubZ8cz3quq"
+  );
+
+  const nftCustody = token.getAssociatedTokenAddressSync(
+    nftMint,
+    nftAuthorityPda,
+    true
   );
 
   const swapFeeLamports = new anchor.BN(0.1 * anchor.web3.LAMPORTS_PER_SOL);
@@ -27,7 +53,11 @@ describe("cercols", () => {
   it("Is initialized!", async () => {
     const tx = await program.methods
       .initPool(swapFeeLamports)
-      .accounts({ collectionMint, pool: poolPda })
+      .accounts({
+        collectionMint,
+        pool: poolPda,
+        nftAuthority: nftAuthorityPda,
+      })
       .rpc();
     console.log("Your transaction signature", tx);
 
@@ -41,5 +71,22 @@ describe("cercols", () => {
       swapFeeLamports.toNumber()
     );
     expect(account.size).to.eq(0);
+  });
+
+  it.skip("Can deposit an NFT from the collection", async () => {
+    const tx = await program.methods
+      .deposit()
+      .accounts({
+        pool: poolPda,
+        nftAuthority: nftAuthorityPda,
+        nftMint,
+        nftToken,
+        nftMetadata,
+        nftEdition,
+        nftCustody,
+      })
+      .rpc();
+
+    console.log("TX: ", tx);
   });
 });
