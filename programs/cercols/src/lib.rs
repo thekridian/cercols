e1a99ed32fd2de9ee5996bfb41305372b29229d5
kridian <thekridian@proton.me>
2024-01-23 19:00:35 -0800

    test refactor

--- programs/cercols/src/lib.rs
+++ programs/cercols/src/lib.rs
@@ -41,10 +41,10 @@ pub mod cercols {
         let nft_mint = &ctx.accounts.nft_mint.to_account_info();
         let nft_metadata = &ctx.accounts.nft_metadata.to_account_info();
         let nft_edition = &ctx.accounts.nft_edition.to_account_info();
-        let auth_rules_program = &ctx.accounts.auth_rules_program.to_account_info();
+        // let auth_rules_program = &ctx.accounts.auth_rules_program.to_account_info();
         // let auth_rules = &ctx.accounts.auth_rules.to_account_info();
         let source_token_record = &ctx.accounts.source_token_record.to_account_info();
-        let destination_token_record = &ctx.accounts.destination_token_record.to_account_info();
+        // let destination_token_record = &ctx.accounts.destination_token_record.to_account_info();
         
         transfer_cpi
         .token(nft_token)
@@ -55,11 +55,11 @@ pub mod cercols {
         .metadata(nft_metadata)
         .edition(Some(nft_edition))
         .token_record(Some(source_token_record))
-        .destination_token_record(Some(destination_token_record))
+        // .destination_token_record(Some(destination_token_record))
         .authority(user)
         .payer(user)
         .system_program(&ctx.accounts.system_program)
-        .authorization_rules_program(Some(auth_rules_program))
+        // .authorization_rules_program(Some(auth_rules_program))
         // .authorization_rules(Some(auth_rules))
         .sysvar_instructions(&ctx.accounts.sysvar_instructions)
         .spl_token_program(&ctx.accounts.metadata_program)
@@ -67,8 +67,6 @@ pub mod cercols {
         .amount(1);
 
         transfer_cpi.invoke()?;
-
-        // transfer(ctx.accounts.deposit_nft_ctx(), 1)?;
 
         Ok(())
     }
@@ -201,18 +199,18 @@ pub struct Deposit<'info> {
     )]
     pub source_token_record: Account<'info, TokenRecordAccount>,
     
-    #[account(
-        mut,
-        seeds = [b"metadata", 
-            Metadata::id().as_ref(),
-            nft_mint.key().as_ref(),
-            b"token_record",
-            nft_custody.key().as_ref(),
-        ],
-        seeds::program = Metadata::id(),
-        bump
-    )]
-    pub destination_token_record: Account<'info, TokenRecordAccount>,
+    // #[account(
+    //     mut,
+    //     seeds = [b"metadata", 
+    //         Metadata::id().as_ref(),
+    //         nft_mint.key().as_ref(),
+    //         b"token_record",
+    //         nft_custody.key().as_ref(),
+    //     ],
+    //     seeds::program = Metadata::id(),
+    //     bump
+    // )]
+    // pub destination_token_record: Account<'info, TokenRecordAccount>,
 
     #[account(mut)]
     pub user: Signer<'info>,
@@ -226,9 +224,9 @@ pub struct Deposit<'info> {
     #[account(address = sysvar::instructions::id())]
     pub sysvar_instructions: UncheckedAccount<'info>,
 
-    /// CHECK: account constraints checked in account trait
-    #[account(address = mpl_token_auth_rules::id())]
-    pub auth_rules_program: UncheckedAccount<'info>,
+    // /// CHECK: account constraints checked in account trait
+    // #[account(address = mpl_token_auth_rules::id())]
+    // pub auth_rules_program: UncheckedAccount<'info>,
 
     // /// CHECK: account constraints checked in account trait
     // #[account(owner = mpl_token_auth_rules::id())]
--- tests/cercols.ts
+++ tests/cercols.ts
@@ -18,21 +18,22 @@ import {
   MPL_TOKEN_METADATA_PROGRAM_ID,
   createNft,
   createProgrammableNft,
+  findEditionMarkerPda,
+  findMasterEditionPda,
   findMetadataPda,
+  findTokenRecordPda,
   mplTokenMetadata,
   verifyCollectionV1,
 } from "@metaplex-foundation/mpl-token-metadata";
 import { readFileSync } from "fs";
 import path from "path";
 import { findAssociatedTokenPda } from "@metaplex-foundation/mpl-toolbox";
+import { MPL_TOKEN_AUTH_RULES_PROGRAM_ID } from "@metaplex-foundation/mpl-token-auth-rules";
 
 describe("cercols", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
-  console.log("provider", anchor.AnchorProvider.defaultOptions());
-
   anchor.setProvider(provider);
-
   const program = anchor.workspace.Cercols as Program<Cercols>;
 
   const umi = createUmi(provider.connection.rpcEndpoint).use(
@@ -51,12 +52,21 @@ describe("cercols", () => {
       path.join(process.env.HOME, ".config/solana/id.json")
     ).toString()
   );
-
   const signer = umi.eddsa.createKeypairFromSecretKey(
     new Uint8Array(keyFileContents)
   );
-
   umi.use(signerIdentity(createSignerFromKeypair(umi, signer)));
+
+  // Programs we need
+  const metadataProgram = new anchor.web3.PublicKey(
+    MPL_TOKEN_METADATA_PROGRAM_ID
+  );
+  const authRulesProgram = new anchor.web3.PublicKey(
+    MPL_TOKEN_AUTH_RULES_PROGRAM_ID
+  );
+  const sysvarInstructions = new anchor.web3.PublicKey(
+    "Sysvar1nstructions1111111111111111111111111"
+  );
 
   // Collection Mint
   const collectionMint = generateSigner(umi);
@@ -75,10 +85,10 @@ describe("cercols", () => {
   const nftTokenPubkey = new anchor.web3.PublicKey(publicKey(nftToken));
 
   const nftMetadata = findMetadataPda(umi, { mint: nftMint2.publicKey });
+  const nftMetadataPubkey = new anchor.web3.PublicKey(publicKey(nftMetadata));
 
-  const metadataProgram = new anchor.web3.PublicKey(
-    MPL_TOKEN_METADATA_PROGRAM_ID
-  );
+  const nftEdition = findMasterEditionPda(umi, { mint: nftMint2.publicKey });
+  const nftEditionPubkey = new anchor.web3.PublicKey(publicKey(nftEdition));
 
   const [poolPda] = anchor.web3.PublicKey.findProgramAddressSync(
     [
@@ -100,34 +110,20 @@ describe("cercols", () => {
     true
   );
 
-  const [sourceTokenRecord] = anchor.web3.PublicKey.findProgramAddressSync(
-    [
-      anchor.utils.bytes.utf8.encode("metadata"),
-      metadataProgram.toBytes(),
-      nftMint2Pubkey.toBytes(),
-      anchor.utils.bytes.utf8.encode("token_record"),
-      nftTokenPubkey.toBytes(),
-    ],
-    program.programId
+  const sourceTokenRecord = findTokenRecordPda(umi, {
+    mint: nftMint2.publicKey,
+    token: publicKey(nftToken),
+  });
+  const sourceTokenRecordPubkey = new anchor.web3.PublicKey(
+    publicKey(sourceTokenRecord)
   );
 
-  const [destinationTokenRecord] = anchor.web3.PublicKey.findProgramAddressSync(
-    [
-      anchor.utils.bytes.utf8.encode("metadata"),
-      metadataProgram.toBytes(),
-      nftMint2Pubkey.toBytes(),
-      anchor.utils.bytes.utf8.encode("token_record"),
-      nftCustody.toBytes(),
-    ],
-    program.programId
-  );
-
-  const authRulesProgram = new anchor.web3.PublicKey(
-    "auth9SigNpDKz4sJJ1DfCTuZrZNSAgh9sFD3rboVmgg"
-  );
-
-  const sysvarInstructions = new anchor.web3.PublicKey(
-    "Sysvar1nstructions1111111111111111111111111"
+  const destinationTokenRecord = findTokenRecordPda(umi, {
+    mint: nftMint2.publicKey,
+    token: publicKey(nftCustody),
+  });
+  const destinationTokenRecordPubkey = new anchor.web3.PublicKey(
+    publicKey(destinationTokenRecord)
   );
 
   const swapFeeLamports = new anchor.BN(0.1 * anchor.web3.LAMPORTS_PER_SOL);
@@ -157,8 +153,6 @@ describe("cercols", () => {
       collectionMint: collectionMint.publicKey,
       authority: umi.payer,
     }).sendAndConfirm(umi, sendAndConfirmOptions);
-
-    // console.log("tx", tx);
   });
 
   // after(async () => {});
@@ -187,25 +181,25 @@ describe("cercols", () => {
     expect(account.size).to.eq(0);
   });
 
-  // it("Can deposit an NFT from the collection", async () => {
-  //   const tx = await program.methods
-  //     .deposit()
-  //     .accounts({
-  //       pool: poolPda,
-  //       nftAuthority: nftAuthorityPda,
-  //       nftMint,
-  //       nftToken,
-  //       nftMetadata,
-  //       nftEdition,
-  //       nftCustody,
-  //       sourceTokenRecord,
-  //       destinationTokenRecord,
-  //       metadataProgram,
-  //       sysvarInstructions,
-  //       authRulesProgram,
-  //     })
-  //     .rpc();
+  it("Can deposit an NFT from the collection", async () => {
+    const tx = await program.methods
+      .deposit()
+      .accounts({
+        pool: poolPda,
+        nftAuthority: nftAuthorityPda,
+        nftMint: nftMint2Pubkey,
+        nftToken: nftTokenPubkey,
+        nftMetadata: nftMetadataPubkey,
+        nftEdition: nftEditionPubkey,
+        nftCustody,
+        sourceTokenRecord: sourceTokenRecordPubkey,
+        // destinationTokenRecord: destinationTokenRecordPubkey,
+        metadataProgram,
+        sysvarInstructions,
+        // authRulesProgram,
+      })
+      .rpc();
 
-  //   console.log("TX: ", tx);
-  // });
+    console.log("TX: ", tx);
+  });
 });
